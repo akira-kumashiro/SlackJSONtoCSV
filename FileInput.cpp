99ccbd5c2f77778dec0384264e91c944c82ec8b4
Akira Kumashiro <34697080+akira-kumashiro@users.noreply.github.com>
2023-09-20 14:07:32 +0900

    Update FileInput.cpp

--- FileInput.cpp
+++ FileInput.cpp
@@ -7,12 +7,7 @@ FileInput::FileInput(std::string _filename)
 
 std::vector<std::vector<double>> FileInput::dataInput(std::string filename)
 {
-	setlocale(LC_ALL, "");
-
 	std::ifstream ifs(filename);
-
-	//static_assert(sizeof(wchar_t) == 2, "error.");//Linuxではつかうcvt違うから直してくれ
-	//ifs.imbue(std::locale(std::locale(""), new std::codecvt_utf8_utf16<wchar_t, 0x10ffff, std::consume_header>()));
 
 	if (!ifs.is_open())
 	{
@@ -25,7 +20,7 @@ std::vector<std::vector<double>> FileInput::dataInput(std::string filename)
 	std::vector<double> result;
 	std::vector<std::vector<double>> fileData;
 
-	std::getline(ifs, line);
+	std::getline(ifs, line);//1行分進める
 	while (std::getline(ifs, line))
 	{
 		std::vector < std::string> strvec = split(line, ',');//1行のデータ
@@ -45,9 +40,7 @@ std::vector<std::string> FileInput::split(std::string& input, char delimiter)
 	std::string field;
 	std::vector<std::string> result;
 	while (std::getline(stream, field, delimiter))
-	{
 		result.push_back(field);
-	}
 
 	return result;
 }
@@ -55,23 +48,9 @@ std::vector<std::string> FileInput::split(std::string& input, char delimiter)
 std::string FileInput::getDate(time_t _time)
 {
 	std::ostringstream converted_date;
-	/*
-	Tue Aug 2 02:13:24 2016
-	Tue Aug 2 03:55:19 2016
-	2016/8/2 3:55:19
-	*/
 
-	// convert time_t to structure tm. You can use instead gmtime() for UTC
-	//tm* tm_previous = localtime(&_time);
-
-	// print time using asctime() function.
-	//std::cout << asctime(tm_previous);
-
-	// for current time
-	//time_t current_time = std::time(0); // get current time
 	tm conv_time;
 	errno_t error = localtime_s(&conv_time, &_time);
-	//std::cout << asctime(tm_current);
 
 	// print by accessing contents of the structure
 	converted_date << conv_time.tm_year + 1900 // year 0 corresponds to 1900
